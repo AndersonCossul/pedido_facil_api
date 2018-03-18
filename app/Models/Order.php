ebf3afe13ddb22419e90aefb1855782be4842fa3
Anderson Cossul <anderson_cossul@hotmail.com>
2018-03-18 00:58:37 -0300

    Order model.

---
+++ app/Models/Order.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'orders';
+    protected $fillabe = [
+        'name',
+        'card_id',
+        'table_id',
+        'waiter_id',
+        'print_status',
+        'total_value',
+        'date',
+        'status'
+    ];
+}
---
+++ database/migrations/2018_03_18_035707_orders_table_migration.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class OrdersTableMigration extends Migration
+{
+    public function up()
+    {
+        Schema::create('orders', function(Blueprint $table) {
+            $table->string('name');
+            $table->integer('card_id');
+            $table->integer('table_id');
+            $table->integer('waiter_id');
+            $table->tinyInteger('print_status');
+            $table->double('total_value');
+            $table->date('date');
+            $table->tinyInteger('status');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
