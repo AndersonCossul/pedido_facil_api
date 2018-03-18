110328816b013d1eb24cc63e1e3df791bbf5343c
Anderson Cossul <anderson_cossul@hotmail.com>
2018-03-18 00:54:49 -0300

    Card model.

---
+++ database/migrations/2018_03_18_035421_cards_table_migration.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CardsTableMigration extends Migration
+{
+    public function up()
+    {
+        Schema::create('cards', function(Blueprint $table) {
+            $table->string('name');
+            $table->tinyInteger('status');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
