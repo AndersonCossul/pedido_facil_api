353cb46ad56a6de42852d6cc8fc091c0527f457a
Anderson Cossul <anderson_cossul@hotmail.com>
2018-03-20 22:44:02 -0300

    CardController.

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Controller for the cards.
+ */
+class CardController extends Controller
+{
+    private $repository;
+
+    /**
+     * Using dependency injection to get the repository connected to the API in the app_providers.
+     *
+     * @param interface $repository
+     */
+    public function __construct($repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Returns all the cards.
+     *
+     * @return array
+     */
+    public function index()
+    {
+        $cards = $this->repository->all();
+        dd($cards);
+    }
+
+    /**
+     * Store a new card.
+     *
+     * @return boolean
+     */
+    public function create($request)
+    {
+        $response = $this->repository->create($request);
+        dd($response);
+    }
+
+     /**
+     * Update a card.
+     *
+     * @return boolean
+     */
+    public function update($request)
+    {
+        $response = $this->repository->update($request);
+        dd($response);
+    }
+
+     /**
+     * Delete a card.
+     *
+     * @return boolean
+     */
+    public function delete($request)
+    {
+        $response = $this->repository->delete($request);
+        dd($response);
+    }
+}
