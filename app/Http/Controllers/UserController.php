539820029550d400bc3dd120cc2728ac21776f49
Anderson Cossul <anderson_cossul@hotmail.com>
2018-03-20 22:46:07 -0300

    UserController.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Controller for the users.
+ */
+class UserController extends Controller
+{
+    private $repository;
+
+    /**
+     * Using dependency injection to get the repository connected to the API in the app_providers.
+     *
+     * @param interface $repository
+     */
+    public function __construct($repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Returns all the users.
+     *
+     * @return array
+     */
+    public function index()
+    {
+        $users = $this->repository->all();
+        dd($users);
+    }
+
+    /**
+     * Store a new user.
+     *
+     * @return boolean
+     */
+    public function create($request)
+    {
+        $response = $this->repository->create($request);
+        dd($response);
+    }
+
+     /**
+     * Update an user.
+     *
+     * @return boolean
+     */
+    public function update($request)
+    {
+        $response = $this->repository->update($request);
+        dd($response);
+    }
+
+     /**
+     * Delete an user.
+     *
+     * @return boolean
+     */
+    public function delete($request)
+    {
+        $response = $this->repository->delete($request);
+        dd($response);
+    }
+}
