0bd506df6085fb2630865565d9df1f6d354edc30
Anderson Cossul <anderson_cossul@hotmail.com>
2018-03-20 22:38:34 -0300

    GroupController

---
+++ app/Http/Controllers/Product/GroupController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Controller for the product groups.
+ */
+class GroupController extends Controller
+{
+    private $repository;
+
+    /**
+     * Using dependency injection to get the repository connected to the API in the app_providers.
+     *
+     * @param interface $repository
+     */
+    public function __construct($repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Returns all the groups.
+     *
+     * @return array
+     */
+    public function index()
+    {
+        $groups = $this->repository->all();
+        dd($groups);
+    }
+
+    /**
+     * Store a new group.
+     *
+     * @return boolean
+     */
+    public function create($request)
+    {
+        $response = $this->repository->create($request);
+        dd($response);
+    }
+
+     /**
+     * Update a group.
+     *
+     * @return boolean
+     */
+    public function update($request)
+    {
+        $response = $this->repository->update($request);
+        dd($response);
+    }
+
+     /**
+     * Delete a group.
+     *
+     * @return boolean
+     */
+    public function delete($request)
+    {
+        $response = $this->repository->delete($request);
+        dd($response);
+    }
+}
