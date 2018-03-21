fd6c96af277ec6247b1c370af7aab2cfceeaf59b
Anderson Cossul <anderson_cossul@hotmail.com>
2018-03-20 22:35:15 -0300

    AdditionalController.

---
+++ app/Http/Controllers/Product/AdditionalController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Controller for the product additionals.
+ */
+class AdditionalController extends Controller
+{
+    private $repository;
+
+    /**
+     * Using dependency injection to get the repository connected to the API in the app_providers.
+     *
+     * @param [type] $repository
+     */
+    public function __construct($repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Returns all the additionals;
+     *
+     * @return array
+     */
+    public function index()
+    {
+        $additionals = $this->repository->all();
+        dd($additionals);
+    }
+
+    /**
+     * Store a new additional;
+     *
+     * @return boolean
+     */
+    public function create($request)
+    {
+        $response = $this->repository->create($request);
+        dd($response);
+    }
+
+     /**
+     * Update an additional;
+     *
+     * @return boolean
+     */
+    public function update($request)
+    {
+        $response = $this->repository->update($request);
+        dd($response);
+    }
+
+     /**
+     * Delete an additional;
+     *
+     * @return boolean
+     */
+    public function delete($request)
+    {
+        $response = $this->repository->delete($request);
+        dd($response);
+    }
+}
