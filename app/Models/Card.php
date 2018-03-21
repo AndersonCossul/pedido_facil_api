b9c33fcf9e1c1d52e1f5461baff01bf17e0a0ba7
Anderson Cossul <anderson_cossul@hotmail.com>
2018-03-20 22:27:12 -0300

    Card doc fix.

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -5,10 +5,10 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * This model represents types of cards.
- * Could be credit cards, debit card, or just money!
+ * Could be credit card, debit card, or just money!
  * 
  * Accepts name: string, status: tinyInteger.
- * Status represents if the actual card is available at the moment.
+ * Status represents if the card is available at the moment.
  */
 class Card extends Model
 {
